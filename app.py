370d53697c374360d7774cabb5641209916cbd51
Munchken105 <tomerhumer7@gmail.com>
2026-05-01 22:51:59 -0400

    pulled my steam achievement data and ranked my achievment completion per game

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import json
-import panda as panda
+import pandas as pd
 import os
 from dotenv import load_dotenv
+import requests
+import plotly.express as px
+
 
 load_dotenv()
 
@@ -13,10 +16,10 @@ STEAM_ID = os.getenv("STEAM_ID")
 #data
 games_url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/" #the base url with no parameters
 param = { #the parameters im going to feed the url 
-    "key": STEAM_API_KEY
-    "steamid" : STEAM_ID
-    "format": "json"
-    "include_appinfo": True
+    "key": STEAM_API_KEY,
+    "steamid" : STEAM_ID,
+    "format": "json",
+    "include_appinfo": True,
     "include_played_free_games": True
 }
 
@@ -36,30 +39,76 @@ achievement_url = "https://api.steampowered.com/ISteamUserStats/GetPlayerAchieve
 #place all achievements in a dict to refernce the data later, key = appid and values = achievements
 achievement_dict = {}
 
+completion_list = []
+
 for game in games:
     appid = game["appid"]
+    game_name = game.get("name", "Unkown Game")
     
     achievement_param = {
-        "key": STEAM_API_KEY
-        "steamid": STEAM_ID
+        "key": STEAM_API_KEY,
+        "steamid": STEAM_ID,
         "appid": appid
     }
 
     #send a request
     response = requests.get(achievement_url, params=achievement_param)
+    
     #parse the request
     achievement_data = response.json()
 
+    if "playerstats" in achievement_data:
+        achievements = achievement_data["playerstats"].get("achievements", [])
 
-    
+        achievement_dict[appid] = {
+            "game_name" : game_name,
+            "achievements" : achievements
+        }
 
+        total_achievements = len(achievements)
+
+        if total_achievements > 0:
+            unlocked = sum(achievement["achieved"] for achievement in achievements)
+            completion_percent = (unlocked / total_achievements) * 100
+
+            completion_list.append({
+                "game_name" : game_name,
+                "appid" : appid,
+                "unlocked" : unlocked,
+                "total" : total_achievements,
+                "completion_percent": completion_percent
+            })
+
+df = pd.DataFrame(completion_list)
+
+df = df.sort_values(by="completion_percent", ascending=False)
+
+fig = px.bar(
+    df.head(10),
+    x = 'completion_percent',
+    y = 'game_name',
+    title = "Steam Achievement Completion Per Game",
+    orientation="h",
+    labels = {
+        "game_name" : "Game",
+        "completion_percent" : "Completion %"
+    },
+    hover_data = ["unlocked", "total"]
+    )
+
+fig.update_layout(yaxis=dict(autorange="reversed"))
 
 
 #inililizae dash app
 app = Dash(__name__)
 
 #app layout
-app.layout = [html.Div(children='Hello World')]
+app.layout = html.Div([
+    html.H1("Steam Achievement Dashboard"),
+    html.P(f"Games checked: {len(achievement_dict)}"),
+    dcc.Graph(figure=fig)
+
+])
 
 # Run the app
 if __name__ == '__main__':
